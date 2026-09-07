1e44eaa220189f9d9d3f7807cd086b7cec91e0fd
dedandy <daniele.debernardi@cimafoundation.org>
2026-09-07 17:32:23 +0200

    chore: bump version to 4.9.23

--- SleepManager.1m.sh
+++ SleepManager.1m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # <bitbar.title>macOS Sleep Manager Monitor</bitbar.title>
-# <bitbar.version>v4.9.22</bitbar.version>
+# <bitbar.version>v4.9.23</bitbar.version>
 # <swiftbar.hideAbout>true</swiftbar.hideAbout>
 # <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
 # <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
